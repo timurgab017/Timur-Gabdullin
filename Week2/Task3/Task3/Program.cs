f4afb2947ab4d4a9d6233d6a60c8b69afc543d57
Timur <timurgab017@mail.ru>
2019-02-12 00:13:36 +0600

    imi

---
+++ Week2/Task3/Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void PrintSpaces(int level)
+        {
+            for (int i = 0; i < level; i++)
+                Console.Write("  ");
+        }
+        static void FD(DirectoryInfo directory, int level)
+        {
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            foreach (FileInfo file in files)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (DirectoryInfo d in directories)
+            {
+                PrintSpaces(level);
+                Console.WriteLine(d.Name);
+                FD(d, level + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo("C:/Users/acer optane/Desktop/PP2labs/Week1");
+            FD(d, 0);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderName = @"C:/Users/acer optane/Desktop";
+            string path = Path.Combine(folderName, "test2");
+            Directory.CreateDirectory(path);
+            string source = @"C:/Users/acer optane/Desktop/test";
+            string[] files = Directory.GetFiles(source);
+            foreach (string file in files)
+            {
+                File.Copy(file, Path.Combine(path, Path.GetFileName(file)));
+                File.Delete(file);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
